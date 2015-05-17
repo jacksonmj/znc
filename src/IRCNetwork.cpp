4ceb7d903e17eb7a6c4fadbc837a64f299194bff
jacksonmj <jacksonmj-github@jacksonmj.co.uk>
2015-05-17 19:18:44 +0000

    Revert "Retire awaynick (resolves #661)"
    This reverts commit 14e09e1a10a62fd911dbef5da8c7f14346e87c8a.

--- src/IRCNetwork.cpp
+++ src/IRCNetwork.cpp
@@ -22,7 +22,6 @@
 #include <znc/Server.h>
 #include <znc/Chan.h>
 #include <znc/Query.h>
-#include <algorithm>
 
 using std::vector;
 using std::set;
@@ -419,19 +418,8 @@ bool CIRCNetwork::ParseConfig(CConfig *pConfig, CString& sError, bool bUpgrade) 
 			bool bModRet = LoadModule(sModName, sArgs, sNotice, sModRet);
 
 			if (!bModRet) {
-				// XXX The awaynick module was retired in 1.6 (still available as external module)
-				if (sModName == "awaynick") {
-					// load simple_away instead, unless it's already on the list
-					if (std::find(vsList.begin(), vsList.end(), "simple_away") == vsList.end()) {
-						sNotice = "Loading network module [simple_away] instead";
-						sModName = "simple_away";
-						// not a fatal error if simple_away is not available
-						LoadModule(sModName, sArgs, sNotice, sModRet);
-					}
-				} else {
-					sError = sModRet;
-					return false;
-				}
+				sError = sModRet;
+				return false;
 			}
 		}
 	}
